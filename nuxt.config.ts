457c456b81e0db0e9c3ec44a30b4515b9014a737
Daniel Nguyen <94246004+CrazEpic@users.noreply.github.com>
2026-04-03 14:15:03 -0500

    test deploy with proper base URL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
 	modules: ["@nuxt/ui"],
 	devtools: { enabled: true },
 	css: ["~/assets/css/main.css"],
+	app: {
+		baseURL: "/Sensible-Unicode/",
+	},
 	vite: {
 		optimizeDeps: {
 			include: ["@vue/devtools-core", "@vue/devtools-kit"],
